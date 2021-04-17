711d482f450e60688d06bff7a452d40ba0173f2c
Sumon Sarker <csesumonpro@gmail.com>
2021-04-18 02:08:23 +0600

    Handling user input

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const counter = {
     data() {
         return {
             title: "This is a brand new vue 3 course",
-            list: ['apple', 'orange', 'mango']
+            list: ['apple', 'orange', 'mango'],
+            textInput: ''
         }
-    }
+    },
+    methods: {
+      submitForm() {
+        this.textInput = this.textInput
+        .split('')
+        .reverse()
+        .join('')
+      }
+    },
   }
   
 const app = Vue.createApp(counter);
